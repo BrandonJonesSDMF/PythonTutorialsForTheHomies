52ca24681ef56a6b50aba80b5e99cfb36f93e733
BrandonJonesSDMF <56842953+BrandonJonesSDMF@users.noreply.github.com>
2020-10-29 22:09:34 -0700

    Rename shape.py to 2_shape.py

--- shape.py
+++
@@ -1,6 +0,0 @@
-print("|\ ")
-print("| \ ")
-print("|  \ ")
-print("|   \ ")
-print("|____\ ")
-'''you can use a print statment to display shapes if you keep a general scope in mind. now you're an artist baby uwu'''
