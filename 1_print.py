a7209a394931f92aaea1692cc8698a6bdfc76443
BrandonJonesSDMF <56842953+BrandonJonesSDMF@users.noreply.github.com>
2020-10-29 22:09:01 -0700

    Rename print.py to 1_print.py

---
+++ 1_print.py
@@ -0,0 +1,2 @@
+print("hello world")
+'''print takes a given string and displays it on your terminal. great job dick head you're a programmer now.'''
--- print.py
+++
@@ -1,2 +0,0 @@
-print("hello world")
-'''print takes a given string and displays it on your terminal. great job dick head you're a programmer now.'''
